38189c992c9486fff5547c57b0145322e99a19fe
linesonlines <91343837+linesonlines@users.noreply.github.com>
2023-05-10 23:46:49 -0500

    Delete Assignment4/Assignment2 directory

--- Assignment4/Assignment2/Assignment2Q1/Item_Type.cpp
+++
@@ -1,5 +0,0 @@
-struct Item_Type {
-    // Each item has an integer value and a pointer to the next item
-    int value;
-    Item_Type* next;
-};
--- Assignment4/Assignment2/Assignment2Q1/Item_Type.h
+++
@@ -1,9 +0,0 @@
-#ifndef ITEM_TYPE_H
-#define ITEM_TYPE_H
-
-struct Item_Type {
-    int value;
-    Item_Type* next;
-};
-
-#endif /* ITEM_TYPE_H */
